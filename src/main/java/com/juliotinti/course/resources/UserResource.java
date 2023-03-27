68313ccab4f75a0f9c2616f731fe3926feb8927f
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-27 09:28:44 -0300

    User insert

--- src/main/java/com/juliotinti/course/resources/UserResource.java
+++ src/main/java/com/juliotinti/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.juliotinti.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juliotinti.course.entities.User;
 import com.juliotinti.course.services.UserService;
@@ -19,7 +23,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@GetMapping
+	@GetMapping //para conseguir usar o Ger no postman
 	public ResponseEntity<List<User>> findAll(){  //ResponseEntities é um tipo Spring para retornar respostas de requisições Web
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list); //o body é para você colocar o q está em parenteses no corpo (na tela) da requisição
@@ -29,6 +33,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){ //PathVariable é para que o parâmetro seja a variavel da requisição, no caso {id} 
 		User user = service.finalById(id);
 		return ResponseEntity.ok().body(user); 
+	}
+	
+	@PostMapping //para conseguir usar o Post no postman
+	public ResponseEntity<User> insert(@RequestBody User user){ //RequestBody é para deserializar um objeto JSON para objeto User
+		user = service.insert(user);
+		//temos que construir esse URI para que o próprio postman nos mostre em qual caminho está o objeto inserido, para vermos no web
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user); 
 	}
 
 }
